5f022af64127d966929b163f6178d4958584d966
Paulina Radomska <paulinaradomska10@wp.pl>
2023-04-16 17:53:15 +0200

    function to generate password and using pyperclip package

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 from tkinter import *
 from tkinter import messagebox
+import random
+import pyperclip
 
 FONT_NAME = "Courier"
 
 
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_passwd():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_numbers = random.randint(2, 4)
+    nr_symbols = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+
+    password_list = password_letters + password_numbers + password_symbols
+
+    random.shuffle(password_list)
+    password = ''.join(password_list)
+
+    entry_passwd.insert(0, password)
+    pyperclip.copy(password)
+
+
 def save_passwd():
     if len(entry_web.get()) == 0 or len(entry_login.get()) == 0 or len(entry_passwd.get()) == 0:
         messagebox.showinfo(title="Ooops", message="Please make sure you haven't left any fields empty.")
@@ -48,7 +73,7 @@ entry_login.grid(row=2, column=1, columnspan=2)
 entry_passwd = Entry(width=37)
 entry_passwd.grid(row=3, column=1)
 
-button_gen = Button(text="Generate", font=(FONT_NAME, 8, "normal"), relief="raised", width=18)
+button_gen = Button(text="Generate", font=(FONT_NAME, 8, "normal"), relief="raised", width=18, command=generate_passwd)
 button_gen.grid(row=3, column=2)
 
 button_add = Button(text="Add", font=(FONT_NAME, 8, "normal"), relief="raised", width=51, command=save_passwd)
