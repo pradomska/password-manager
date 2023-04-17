67a471c77982a47713b9a05bdba7d54c7122216e
Paulina Radomska <paulinaradomska10@wp.pl>
2023-04-17 19:08:39 +0200

    function find password

--- main.py
+++ main.py
@@ -7,6 +7,26 @@ import json
 FONT_NAME = "Courier"
 
 
+def find_passwd():
+    website = entry_web.get()
+
+    if len(website) == 0:
+        messagebox.showinfo(title="MessageBox", message="Enter a website")
+    else:
+        try:
+            with open("passwd.json", mode="r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            messagebox.showinfo(title="Error", message="No Data File Found.")
+        else:
+            if website in data:
+                login = data[website]["login"]
+                password = data[website]["password"]
+                messagebox.showinfo(title=website, message=f"Login: {login}\nPassword: {password}")
+            else:
+                messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
+
+
 def generate_passwd():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
@@ -32,7 +52,6 @@ def generate_passwd():
 
 
 def save_passwd():
-
     website = entry_web.get()
     login = entry_login.get()
     password = entry_passwd.get()
@@ -45,13 +64,17 @@ def save_passwd():
     if len(website) == 0 or len(login) == 0 or len(password) == 0:
         messagebox.showinfo(title="Ooops", message="Please make sure you haven't left any fields empty.")
     else:
-        with open("passwd.json", mode="r") as data_file:
-            data = json.load(data_file)
+        try:
+            with open("passwd.json", mode="r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("passwd.json", mode="w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
             data.update(new_data)
-
-        with open("passwd.json", mode="w") as data_file:
-            json.dump(data, data_file, indent=4)
-            
+            with open("passwd.json", mode="w") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
             entry_web.delete(0, END)
             entry_passwd.delete(0, END)
 
@@ -74,9 +97,9 @@ label_login.grid(row=2, column=0)
 label_passwd = Label(text="Password:", font=FONT_NAME)
 label_passwd.grid(row=3, column=0)
 
-entry_web = Entry(width=60)
+entry_web = Entry(width=37)
 entry_web.focus()
-entry_web.grid(row=1, column=1, columnspan=2)
+entry_web.grid(row=1, column=1)
 
 entry_login = Entry(width=60)
 entry_login.insert(END, "example@email.com")
@@ -87,6 +110,9 @@ entry_passwd.grid(row=3, column=1)
 
 button_gen = Button(text="Generate", font=(FONT_NAME, 8, "normal"), relief="raised", width=18, command=generate_passwd)
 button_gen.grid(row=3, column=2)
+
+button_search = Button(text="Search", font=(FONT_NAME, 8, "normal"), relief="raised", width=18, command=find_passwd)
+button_search.grid(row=1, column=2)
 
 button_add = Button(text="Add", font=(FONT_NAME, 8, "normal"), relief="raised", width=51, command=save_passwd)
 button_add.grid(row=4, column=1, columnspan=2)
